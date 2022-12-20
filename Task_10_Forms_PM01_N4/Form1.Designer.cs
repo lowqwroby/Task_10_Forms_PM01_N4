725c0f2c53d236edcdb36895ee0cb42b6be38cf8
lowqwroby <eltiobvs@gmail.com>
2022-12-20 20:45:33 +0300

    Добавьте файлы проекта.

---
+++ Task_10_Forms_PM01_N4/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Task_10_Forms_PM01_N4
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(535, 16);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "При нажатии на кнопку выведится порядок действий, выполненных в программе";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 28);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox1.Size = new System.Drawing.Size(534, 243);
+			this.textBox1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(439, 277);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(107, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Вывести";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(573, 315);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Работа с файлами и директориями";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
---
+++ Task_10_Forms_PM01_N4/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace Task_10_Forms_PM01_N4
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		static void z10(string Path, TextBox textBox1)
+		{
+			if (Directory.Exists(@"C:\temp\ALL"))
+			{
+				MessageBox.Show("Вероятно, данная программа уже выполнялась,\nпожалуйста, удалите папку temp, чтобы программа работала корректно.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+			textBox1.Text = "";
+			//1 задание
+			Directory.CreateDirectory(Path + @"\K1");
+			textBox1.Text += String.Format("Папка K1 создана.\r\n");
+			Directory.CreateDirectory(Path + @"\K2");
+			textBox1.Text += String.Format("Папка K2 создана.\r\n\r\n");
+			//2 задание
+			File.Create(Path + @"\K1\t1.txt").Close();
+			textBox1.Text += String.Format("Файл t1.txt создан.\r\n");
+			string t1 = "Мокеева Дарья Алексеевна, 2004 года рождения, место жительства г. Владимир.\n";
+			File.WriteAllText(Path + @"\K1\t1.txt", t1);
+			textBox1.Text += String.Format("В файл t1.txt записан текст.\r\n");
+			File.Create(Path + @"\K1\t2.txt").Close();
+			textBox1.Text += String.Format("Файл t2.txt создан.\r\n");
+			string t2 = "Мальцева Ксения Александровна, 1992 года рождения, место жительства г. Москва.\n";
+			File.WriteAllText(Path + @"\K1\t2.txt", t2);
+			textBox1.Text += String.Format("В файл t2.txt записан текст.\r\n\r\n");
+			//3 задание
+			File.Create(Path + @"\K2\t3.txt").Close();
+			textBox1.Text += String.Format("Файл t3.txt был создан.\r\n");
+			File.WriteAllText(Path + @"\K2\t3.txt", File.ReadAllText(Path + @"\K1\t1.txt"));
+			File.AppendAllText(Path + @"\K2\t3.txt", File.ReadAllText(Path + @"\K1\t2.txt"));
+			textBox1.Text += String.Format("В файл t3.txt записан текст.\r\n\r\n");
+			//4 задание
+			FileInfo f1 = new FileInfo(@"C:\temp\K1\t1.txt");
+			FileInfo f2 = new FileInfo(@"C:\temp\K1\t2.txt");
+			FileInfo f3 = new FileInfo(@"C:\temp\K2\t3.txt");
+			textBox1.Text += String.Format($"Имя файла: {f1.Name}\r\nВремя создания: {f1.CreationTime}\nРазмер: {f1.Length}\r\n");
+			textBox1.Text += String.Format($"Имя файла: {f2.Name}\r\nВремя создания: {f2.CreationTime}\nРазмер: {f2.Length}\r\n");
+			textBox1.Text += String.Format($"Имя файла: {f3.Name}\r\nВремя создания: {f3.CreationTime}\nРазмер: {f3.Length}\r\n\r\n");
+			//5-6 задание
+			f2.MoveTo(@"C:\temp\K2\t2.txt");
+			textBox1.Text += String.Format("Файл t2.txt был перемещен в папку K2\r\n");
+			f1.CopyTo(@"C:\temp\K2\t1.txt");
+			textBox1.Text += String.Format("Файл t1.txt был скопирован в папку K2\r\n\r\n");
+			//7 задание
+			Directory.Move(@"C:\temp\K2", @"C:\temp\ALL");
+			textBox1.Text += String.Format("Папка K2 была переименована в All.\r\n");
+			File.Delete(@"C:\temp\K1\t1.txt");
+			Directory.Delete(@"C:\temp\K1");
+			textBox1.Text += String.Format("Папка K1 была удалена\r\n\r\n");
+			//8 Задание
+			DirectoryInfo dinf = new DirectoryInfo(@"C:\temp\ALL");
+			FileInfo[] finf = dinf.GetFiles();
+
+			foreach (FileInfo f in finf)
+			{
+				textBox1.Text += String.Format("Полное имя файла: " + f.FullName.ToString() + "\r\n");
+				textBox1.Text += String.Format("Атрибуты: " + f.Attributes.ToString()) + "\r\n";
+				textBox1.Text += String.Format("Существует ли файл: " + f.Exists.ToString()) + "\r\n";
+				textBox1.Text += String.Format("Размер файла: " + f.Length.ToString()) + " байт" + "\r\n";
+				textBox1.Text += String.Format("Расширение файла: " + f.Extension.ToString() + "\r\n\r\n");
+			}
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				string path = @"C:\temp";
+				if (Directory.Exists(path))
+				{
+					z10(path, textBox1);
+					
+				}
+				else
+				{
+					Directory.CreateDirectory(path);
+					z10(path, textBox1);
+				}
+			}
+			catch
+			{
+				MessageBox.Show("Неизвестная ошибка!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+	}
+}
